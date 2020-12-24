d65aa030b3bc46c4037b08d4f824b6ba74f55659
Lord Vertice <lordvertice333@gmail.com>
2020-12-24 21:35:51 +0100

    New slash cmds
    Updated discord.js and started to work on slash commands.

--- events/ready.js
+++ events/ready.js
@@ -4,4 +4,22 @@ module.exports = (client) => {
 		'Welcome back! All systems are nominal.\n'+
 		'Command and error log:\n'
 	);
+	client.api.applications(client.user.id).guilds(654392586861281292).commands.post({
+    	data: {
+    	    name: "help",
+    	    description: "I'll show you what I can do~"
+		}
+	});
+	client.api.applications(client.user.id).guilds(654392586861281292).commands.post({
+        data: {
+            name: "ping",
+    	    description: "Pong! Use this to check if I'm alright."
+		}
+	});
+	client.api.applications(client.user.id).guilds(654392586861281292).commands.post({
+        data: {
+            name: "set",
+    	    description: "Change my variable configuration."
+		}
+	});
 }
