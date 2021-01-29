5646c978206dc3195849b549605613cadbe24fd5
Lord Vertice <lordvertice333@gmail.com>
2021-01-29 11:42:12 +0100

    2.0.0-alpha16
    Fixed memory leak caused by adding a new event listener whenever :cricket: is called. Now the listeners will only work once

--- modules/crickets.js
+++ modules/crickets.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
     client.crickets = async msg => {
         let responded = false;
-        client.on('message', message => {
+        client.once('message', message => {
             if (message.channel === msg.channel) responded = true;
         });
         await client.sleep(60000);
