8d79b598cae7894b9b53b4ccad5de628a4ebfa95
Lord Vertice <lordvertice333@gmail.com>
2021-02-01 10:20:29 +0100

    2.0.0-alpha19
    Index file is now asynchronous

--- index.js
+++ index.js
@@ -18,45 +18,49 @@ console.log(
     'Initializing module loader...'
 );
 
-fs.readdir('./events/', (err, files) => {
-    if (err) return console.error(err);
-    console.log('Loading event files...');
-    files.forEach(file => {
-        if (!file.endsWith('.js')) return;
-        const eventName = file.split('.')[0];
-        console.log(`Attempting to load event "${eventName}"`);
-        const event = require(`./events/${file}`);
-        client.on(eventName, event.bind(null, client));
+const init = async () => {
+    fs.readdir('./events/', (err, files) => {
+        if (err) return console.error(err);
+        console.log('Loading event files...');
+        files.forEach(file => {
+            if (!file.endsWith('.js')) return;
+            const eventName = file.split('.')[0];
+            console.log(`Attempting to load event "${eventName}"`);
+            const event = require(`./events/${file}`);
+            client.on(eventName, event.bind(null, client));
+        });
+        console.log('Done.');
     });
-    console.log('Done.');
-});
 
-client.commands = new Enmap();
-client.settings = new Enmap({ name: 'settings' });
+    client.commands = new Enmap();
+    client.settings = new Enmap({ name: 'settings' });
 
-fs.readdir('./commands/', (err, files) => {
-    if (err) return console.error(err);
-    console.log('Loading command files...');
-    files.forEach(file => {
-        if (!file.endsWith('.js')) return;
-        const commandName = file.split('.')[0];
-        console.log(`Attempting to load command "${commandName}"`);
-        const command = require(`./commands/${file}`);
-        client.commands.set(commandName, command);
+    fs.readdir('./commands/', (err, files) => {
+        if (err) return console.error(err);
+        console.log('Loading command files...');
+        files.forEach(file => {
+            if (!file.endsWith('.js')) return;
+            const commandName = file.split('.')[0];
+            console.log(`Attempting to load command "${commandName}"`);
+            const command = require(`./commands/${file}`);
+            client.commands.set(commandName, command);
+        });
+        console.log('Done.');
     });
-    console.log('Done.');
-});
 
-fs.readdir('./modules/', (err, files) => {
-    if (err) return console.error(err);
-    console.log('Loading other module files...');
-    files.forEach(file => {
-        if (!file.endsWith('.js')) return;
-        const moduleName = file.split('.')[0];
-        console.log(`Attempting to load module "${moduleName}"`);
-        require(`./modules/${file}`)(client);
+    fs.readdir('./modules/', (err, files) => {
+        if (err) return console.error(err);
+        console.log('Loading other module files...');
+        files.forEach(file => {
+            if (!file.endsWith('.js')) return;
+            const moduleName = file.split('.')[0];
+            console.log(`Attempting to load module "${moduleName}"`);
+            require(`./modules/${file}`)(client);
+        });
+        console.log('Done.');
     });
-    console.log('Done.');
-});
 
-client.login(config.token);
+    client.login(config.token);
+};
+
+init();
