a64ae1f30c1724037070497670fb321307ba1542
Lord Vertice <lordvertice333@gmail.com>
2021-02-03 14:19:39 +0100

    2.0.0-beta3
    Fix prefix command (may not create override when new value = default)

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,7 +1,8 @@
 exports.run = async (client, message, args) => {
+    const settings = message.settings;
     const joinedValue = args.join(' ');
     if (joinedValue.length < 1) return message.channel.send('Please specify a new value');
-    if (joinedValue === client.settings.prefix) return message.channel.send('This setting already has that value!');
+    if (joinedValue === settings.prefix) return message.channel.send('This setting already has that value!');
 
     if (!client.settings.has(message.guild.id)) client.settings.set(message.guild.id, {});
 
